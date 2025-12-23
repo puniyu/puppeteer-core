a5177ce7c692a8d08469d104637832986376ffa5
wuliya <3369906077@qq.com>
2025-12-23 22:58:28 +0800

    chore(build): 更新构建配置和依赖
    - 更新 package.json 中的 exports 配置，移除 require 字段，修改 import
      路径从 .mjs 到 .js
    - 升级 tsdown 依赖从 0.12.9 到 0.18.2
    - 添加 .github/renovate.json 配置文件，集成 Renovate bot 配置

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,53 +1,30 @@
-import { defineConfig, type Options } from 'tsdown'
-import fs from 'node:fs'
-import path, { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { defineConfig } from "tsdown";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
+const filePath = dirname(fileURLToPath(import.meta.url));
 
-export const options: Options =({
-  entry: ['src/index.ts'],      // 入口文件
-  format: ['esm'],       // ESM格式
-  nodeProtocol: true, // 转换node:protocol
-  unbundle: false,                 // 保持目录结构
-  fixedExtension: true,             // 统一扩展名
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  unbundle: false,
   dts: {
-    resolve: true
-  },                    // 生成类型声明文件
-  clean: true,                  // 清理dist目录
-  minify: true,                 // 压缩生产环境代码
-  target: 'node22',             // 指定ECMAScript目标版本
-  sourcemap: false,              // 生成sourcemap
-  treeshake: true,              // 启用树摇优化
-  platform: 'node',            // 指定为Node.js环境
-  outputOptions: {
-    exports: 'named',
+    resolve: true,
   },
-  outDir: 'dist',               // 指定输出目录 
-  noExternal: ['puppeteer-core'],
-  hooks(hooks) {
-    hooks.hook('build:done', () => {
-      copyFiles()
-    })
+  outExtensions: (_) => {
+    return {
+      js: ".js",
+      dts: ".d.ts",
+    };
   },
-})
-
-
-export default defineConfig(options)
-
-const copyFiles = () => {
-  const file_name_path = fileURLToPath(import.meta.url)
-  const file_path = dirname(file_name_path)
-
-  const distDir = path.join(file_path, 'dist')
-
-  // 删除 .d.cts 文件
-  fs.readdirSync(distDir).forEach((file) => {
-    if(file.endsWith('.d.mts')){
-      const oldPath = path.join(distDir, file)
-      const newPath = path.join(distDir, file.replace('.d.mts', '.d.ts'))
-      fs.renameSync(oldPath, newPath)
-    }
-  })
-
-  console.log('构建目录成功!')
-}
+  nodeProtocol: true,
+  clean: true,
+  minify: true,
+  shims: true,
+  target: "node22",
+  sourcemap: false,
+  treeshake: true,
+  platform: "node",
+  outDir: join(filePath, "dist"),
+  noExternal: ["puppeteer-core"],
+});
